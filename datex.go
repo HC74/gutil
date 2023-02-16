7fdc974a32306d8e3684dd7de5a043efbee3130b
HC74 <1792096481@qq.com>
2023-02-16 18:02:19 +0800

    完善日期和字符串工具

--- datex.go
+++ datex.go
@@ -4,10 +4,12 @@ import "time"
 
 // Now 获取当前时间 默认为 YYYY-MM-dd HH:ss:mm 格式
 func Now() string {
-	return ""
+	timeNow := time.Now().Format("2006-01-02 15:04:05")
+	return timeNow
 }
 
 // ToUnifiedTime 统一返回 YYYY-MM-dd HH:ss:mm
 func ToUnifiedTime(v time.Time) string {
-	return ""
+	dateTimeStr := v.Format("2006-01-02 15:04:05")
+	return dateTimeStr
 }
--- strings.go
+++ strings.go
@@ -1,6 +1,12 @@
 package gutil
 
-// StringIsEmpty 判空
-func StringIsEmpty(v interface{}) bool {
+import "strings"
+
+// StringIsEmpty 判断字符串是否为空
+func StringIsEmpty(v string) bool {
+	newName := strings.TrimSpace(v)
+	if len(newName) == 0 {
+		return true
+	}
 	return false
 }
